8bef4e21969f237255523e40c97e5115accc970a
OFFERSHAHAR <poolsys1@gmail.com>
2026-05-13 22:59:15 +0300

    Update home screen logo

--- scripts/ensure-dist-index.mjs
+++ scripts/ensure-dist-index.mjs
@@ -1,3 +1,16 @@
 import { copyFile } from "node:fs/promises";
 
 await copyFile("dist/src-index.html", "dist/index.html");
+
+await Promise.all(
+  [
+    "manifest.json",
+    "OneSignalSDKWorker.js",
+    "favicon-32.png",
+    "apple-touch-icon.png",
+    "icon-180.png",
+    "icon-192.png",
+    "icon-512.png",
+    "icon-1024.png"
+  ].map((file) => copyFile(file, `dist/${file}`))
+);
